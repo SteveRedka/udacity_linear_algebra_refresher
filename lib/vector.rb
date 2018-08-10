316ced93baccf17cde283951143430ed3dd5611a
Steve Redka <cogitofallacy@gmail.com>
2018-08-10 23:56:54 +0300

    initialize library

---
+++ lib/vector.rb
@@ -0,0 +1,45 @@
+# My implementation of class vector for Udacity's Linear Algebra Refresher
+# course
+class Vector
+  attr_accessor :coordinates, :length
+  def initialize(coordinates)
+    @coordinates = coordinates
+    @length = coordinates.length
+  end
+
+  def +(other)
+    raise 'It isn`t a vector!' if other.class != self.class
+    raise 'These vectors have different dimenstions' if other.length != length
+    new_array = [other.coordinates, coordinates].transpose
+                                                .map { |x| x.reduce(:+) }
+    Vector.new(new_array)
+  end
+
+  def negative
+    Vector.new(coordinates.map(&:-@))
+  end
+
+  def -(other)
+    raise 'It isn`t a vector!' if other.class != self.class
+    raise 'These vectors have different dimenstions' if other.length != length
+    self + other.negative
+  end
+
+  def ==(other)
+    raise 'It isn`t a vector!' if other.class != self.class
+    raise 'These vectors have different dimenstions' if other.length != length
+    coordinates == other.coordinates
+  end
+
+  def scale(int)
+    Vector.new(coordinates.map! { |num| num * int })
+  end
+
+  def inspect
+    @coordinates
+  end
+
+  def to_s
+    "Vector: #{@coordinates}"
+  end
+end
---
+++ quiz.rb
@@ -0,0 +1,19 @@
+require './lib/vector'
+# require 'test/unit'
+
+# p Vector.new([8.218, -9.341]) + Vector.new([-1.129, 2.111])
+
+# class TestVector < Test::Unit::TestCase
+#   def test_simple
+#     vector_sum = Vector.new([8.218, -9.341]) + Vector.new([-1.129, 2.111])
+#     assert_equal(Vector.new([7.089, -7.229999999999999]), vector_sum)
+#   end
+# end
+
+vector_sum = Vector.new([8.218, -9.341]) + Vector.new([-1.129, 2.111])
+vector_diff = Vector.new([7.119, 8.215]) - Vector.new([-8.223, 0.878])
+vector_scale = Vector.new([1.671, -1.012, -0.318]).scale(7.41)
+
+p "Vector sum: #{vector_sum}"
+p "Vector diff: #{vector_diff}"
+p "Vector scale: #{vector_scale}"
